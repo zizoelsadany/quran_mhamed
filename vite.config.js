b4233f13ad89e0b6005d9782d8529786825abce5
zizoelsadany <zizoelsadany7@gmail.com>
2026-04-02 23:57:00 +0200

    update project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Quran Recitation App',
+        short_name: 'Quran App',
+        description: 'Quran recitation application by Sheikh Mohamed Ahmed Hassan',
+        theme_color: '#863bff',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
